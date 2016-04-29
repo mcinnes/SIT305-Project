8fabfb79eef98125a9c9a70284714f2daaab4cd4
stewartfullard <studog_@hotmail.com>
2016-04-30 09:37:00 +1000

    New View Controllers Driver and Register Committing now because i just accidentally deleted a heap of work and starting over :(

---
+++ University Car Pooling/DriverRegisterViewController.m
@@ -0,0 +1,37 @@
+//
+//  DriverRegisterViewController.m
+//  University Car Pooling
+//
+//  Created by Stewart Fullard on 30/04/2016.
+//  Copyright © 2016 Matt McInnes. All rights reserved.
+//
+
+#import "DriverRegisterViewController.h"
+
+@interface DriverRegisterViewController ()
+
+@end
+
+@implementation DriverRegisterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ University Car Pooling/RegisterViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.h
+//  University Car Pooling
+//
+//  Created by Stewart Fullard on 30/04/2016.
+//  Copyright © 2016 Matt McInnes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterViewController : UIViewController
+
+@end
