a3808b384f0c0256c7957b9b83f7086358b6d632
stewartfullard <studog_@hotmail.com>
2016-04-30 12:28:13 +1000

    AboutViewController

---
+++ University Car Pooling/SettingsViewController.m
@@ -0,0 +1,37 @@
+//
+//  SettingsViewController.m
+//  University Car Pooling
+//
+//  Created by Stewart Fullard on 30/04/2016.
+//  Copyright © 2016 Matt McInnes. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
